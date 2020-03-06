35cd4867ec684edd3e5f6f097673670a24f792c1
Jonathan Sinovassin-naik <jsinovassinnaik@jahia.com>
2020-03-06 16:13:45 +0100

    BACKLOG-12650 : add template layout for advanced options tab

---
+++ src/javascript/EditPanel/AdvancedOptions/AdvancedOptions.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {LayoutModule} from '@jahia/moonstone';
+import AdvancedOptionsNavigation from './AdvancedOptionsNavigation/AdvancedOptionsNavigation';
+import AdvancedOptionsContent from './AdvancedOptionsContent/AdvancedOptionsContent';
+import {useContentEditorConfigContext} from '~/ContentEditor.context';
+import classes from './AdvancedOptions.scss';
+
+export const AdvancedOptions = () => {
+    const {nodeData, siteInfo, formik} = useContentEditorConfigContext();
+
+    return (
+        <>
+            <div className={classes.container}>
+                <LayoutModule
+                    navigation={<AdvancedOptionsNavigation nodeData={nodeData}
+                                                           siteInfo={siteInfo}
+                                                           formik={formik}/>}
+                    // TODO BACKLOG-12653 handle action to change view
+                    content={<AdvancedOptionsContent/>}
+                />
+            </div>
+        </>
+    );
+};
+
+export default AdvancedOptions;
---
+++ src/javascript/EditPanel/AdvancedOptions/AdvancedOptionsContent/AdvancedOptionsContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './AdvancedOptionsContent.scss';
+
+export const AdvancedOptionsContent = () => {
+    return (
+        <>
+            <div className={classes.container}>
+                Temp
+            </div>
+        </>
+    );
+};
+
+export default AdvancedOptionsContent;
